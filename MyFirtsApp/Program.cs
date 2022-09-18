f45cf4b60e03c654be806d6a029e76088f4b0278
msuapkg <msuapkg@gmail.com>
2022-09-18 17:39:17 -0500

    Agregar archivos de proyecto.

---
+++ MyFirtsApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Explicación de variables
+            // Se pueden 
+            
+
+        }
+    }
+}
